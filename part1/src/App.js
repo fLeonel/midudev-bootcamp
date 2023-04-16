62041e9cb7be27cb5a812325eb68497b36c301d4
Dennis <dr04860@gmail.com>
2023-04-16 12:40:49 -0600

    first commit

---
+++ part1/src/App.js
@@ -0,0 +1,36 @@
+import Mensaje from './Mensajes.js';
+
+const Description = () => {
+  return <h1>
+    Dias Dennis, espero estes bien. 
+    </h1>
+}
+
+const App = (props) => {
+  return (
+  <div className="App">
+    <Mensaje color='red' message='Estamos trabajando' />
+    <Mensaje color='Yellow' message='En un curso de React'/>
+    <Mensaje color='blue' message='XD...' />
+    <Mensaje color='blue' message='Solo es un texto random' />
+    <Description color='black'  />
+  </div>
+  )
+}
+
+const Dennis ={
+  name:'Dennis Ramirez',
+  age: 22,
+  education: 'PhD',
+  greet: function(){
+    console.log("Hi, it's " + this.name)
+  },
+}
+Dennis.growOlder = function(){
+  this.age += 1
+}
+setTimeout(Dennis.greet.bind(Dennis), 1000)
+
+
+export default App;
+
---
+++ web-ejemplo/script.js
@@ -0,0 +1,22 @@
+// const list =[]
+// const anotherList = list.concat(157)
+
+// const persona = {
+//     name: 'Dennis',
+//     twitter: '@RootOctopus',
+//     age:22,
+//     isDeveloper: true,
+//     links: ['https://rootoctopus.com']
+// }
+
+// document.write(persona.name);
+// document.write(persona.links);
+
+// const field = 'twitter'
+
+// document.write(persona[field])
+// document.write(persona['field']);
+const sumar = (a, b) => {
+    return  a + b
+}
+document.write(sumar(2, 2))
